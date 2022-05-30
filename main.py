d228d3e1fe7330a75e6e86d55982cb90a46a3b32
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-05-29 22:56:38 -0300

    alteracao do formato dos dados no csv

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import numpy as np
 import pandas as pd
 
+def removeSemDados(respostas):
+    return list(filter(lambda x: x != 'Sem dados', respostas))
+    
+
+
 def main():
-    df = pd.read_csv('data/teste.csv', delimiter=';')
+    df = pd.read_csv('data/data_pesquisa.csv', delimiter=';')
     df.replace(np.nan, 'Sem dados', inplace=True)
     print(df)
 
+    listAllresp = []
+
     for index, row in df.iterrows():
         listResps = row.values.tolist()
-        print(listResps)
-        if(index > 1):
-            break
+        listResps = removeSemDados(listResps)
+        # print(listResps)
+        listAllresp.append(listResps)
 
+    print(listAllresp.__len__())
+    print(listAllresp[0])
 main()
